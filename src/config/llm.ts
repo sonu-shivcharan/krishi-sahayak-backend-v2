711d001e61baf41f2f1b7269623ce8999ddb82c0
sonu-shivcharan <sonushivcharan@outlook.com>
2026-01-20 22:06:20 +0530

    change env name

--- src/config/llm.ts
+++ src/config/llm.ts
@@ -2,5 +2,5 @@ import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 
 export const llm = new ChatGoogleGenerativeAI({
   model: "gemini-2.5-flash",
-  apiKey: process.env.GOOGLE_API_KEY!,
+  apiKey: process.env.GOOGLE_GEMINI_API_KEY!,
 });
