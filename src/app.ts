30a6cbb2e1678c9b22597005ef0e63ea1f2fd417
sonu-shivcharan <sonushivcharan@outlook.com>
2026-02-18 23:03:02 +0530

    feat: move cors config to the top

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,18 @@ import morgan from "morgan";
 
 export const app = express();
 
+// CORS configuration
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN?.split(","),
+  credentials: true,
+  methods: ["POST", "PATCH", "PUT", "DELETE", "OPTIONS", "GET"],
+  allowedHeaders: ["Authorization", "Content-Type", "X-Requested-With"],
+  exposedHeaders: ["Content-Type"],
+  optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
+
 // basic config
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
@@ -30,39 +42,27 @@ app.use(
   }),
 );
 
-// CORS configuration
-const corsOptions = {
-  origin: process.env.CORS_ORIGIN?.split(","),
-  credentials: true,
-  methods: ["POST", "PATCH", "PUT", "DELETE", "OPTIONS", "GET"],
-  allowedHeaders: ["Authorization", "Content-Type", "X-Requested-With"],
-  exposedHeaders: ["Content-Type"],
-  optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
-};
-
-app.use(cors(corsOptions));
-
 // Ensure CORS headers are set on all responses (including errors)
-app.use((req: Request, res: Response, next: NextFunction) => {
-  const origin = req.headers.origin;
-  const allowedOrigins = process.env.CORS_ORIGIN?.split(",") || [
-    "http://localhost:5173",
-  ];
+// app.use((req: Request, res: Response, next: NextFunction) => {
+//   const origin = req.headers.origin;
+//   const allowedOrigins = process.env.CORS_ORIGIN?.split(",") || [
+//     "http://localhost:5173",
+//   ];
 
-  if (origin && allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "POST, PATCH, PUT, DELETE, OPTIONS, GET",
-    );
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Authorization, Content-Type, X-Requested-With",
-    );
-  }
-  next();
-});
+//   if (origin && allowedOrigins.includes(origin)) {
+//     res.setHeader("Access-Control-Allow-Origin", origin);
+//     res.setHeader("Access-Control-Allow-Credentials", "true");
+//     res.setHeader(
+//       "Access-Control-Allow-Methods",
+//       "POST, PATCH, PUT, DELETE, OPTIONS, GET",
+//     );
+//     res.setHeader(
+//       "Access-Control-Allow-Headers",
+//       "Authorization, Content-Type, X-Requested-With",
+//     );
+//   }
+//   next();
+// });
 
 // Clerk middleware - must be added before routes
 // This automatically verifies JWT tokens and adds req.auth
