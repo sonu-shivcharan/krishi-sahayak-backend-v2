4b8b87127943c4e317f8aa86cffdd7d0eed9d7d0
sonu-shivcharan <sonushivcharan@outlook.com>
2026-02-20 16:36:56 +0530

    add collections object

--- src/utils/qdrantStore.ts
+++ src/utils/qdrantStore.ts
@@ -5,5 +5,9 @@ export const qdrantClient = new QdrantClient({
   url: process.env.QDRANT_URL!,
   apiKey: process.env.QDRANT_API_KEY!,
 });
-
+// constants/vectorCollections.ts
+export const QDRANT_COLLECTIONS = {
+  FORWARDED_QUERY_ANSWERS: "forwarded_query_answers",
+  COLLECTION_NAME: "krishi_sahayak",
+};
 export const COLLECTION_NAME = "krishi_sahayak";
