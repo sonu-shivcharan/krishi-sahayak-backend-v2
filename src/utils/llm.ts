e7d33f69a47ef5055e9f936acb89da5684227019
sonu-shivcharan <sonushivcharan@outlook.com>
2026-04-18 22:07:42 +0530

    chnage model to gpt oss 20b

--- src/utils/llm.ts
+++ src/utils/llm.ts
@@ -46,6 +46,6 @@ export const llm =
   process.env.LLM_PROVIDER === "google"
     ? getLLM({ model: "gemini-2.5-flash", provider: "google" })
     : getLLM({
-        model: "meta-llama/llama-4-scout-17b-16e-instruct",
+        model: "openai/gpt-oss-20b",
         provider: "groq",
       });
